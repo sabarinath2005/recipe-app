58b58f3fe09bb81bd3a8d33a45179fd869b4a1b1
sabarinath2005 <165458612+sabarinath2005@users.noreply.github.com>
2024-05-06 20:21:05 +0530

    recipe-app

--- src/components/AddRecipe.jsx
+++ src/components/AddRecipe.jsx
@@ -8,23 +8,23 @@ const AddRecipe = () => {
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <div className="row g-3">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <label htmlFor="" className="form-label">recipe title</label>
+                        <label htmlFor="" className="form-label">Recipe Title</label>
                         <input type="text" className="form-control" placeholder='enter recipe title' />
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label htmlFor="" className="form-label">the creator</label>
+                    <label htmlFor="" className="form-label">Creator</label>
                         <input type="text" className="form-control" placeholder='enter the creator'/>
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label htmlFor="" className="form-label">a description</label>
+                    <label htmlFor="" className="form-label">Description</label>
                     <textarea name="" id="" cols="30" rows="10" className="form-control" placeholder='enter a description'></textarea>
                      </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label htmlFor="" className="form-label">image</label>
+                    <label htmlFor="" className="form-label">Image</label>
                     <input type="file" name="" id="" className="form-control" />
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label htmlFor="" className="form-label">recipe type</label>
+                    <label htmlFor="" className="form-label">Recipe Type</label>
                        <select name="" id="" className="form-select">
                        <option value="select recipe type">select recipe type</option>
                         <option value="1">vegetarian</option>
@@ -32,7 +32,7 @@ const AddRecipe = () => {
                        </select>
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label htmlFor="" className="form-label">ingredients</label>
+                    <label htmlFor="" className="form-label">Ingredients</label>
                         <input type="text" className="form-control" placeholder='enter ingredients' />
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
